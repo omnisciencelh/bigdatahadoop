4a91051144555fd8be59fca77abba4391bc5ade5
笙箫语陌 <3453479469@qq.com>
2023-05-01 09:42:24 +0800

    初始化

---
+++ src/main/java/com/lh/bigdatahadoop/BigdatahadoopApplication.java
@@ -0,0 +1,13 @@
+package com.lh.bigdatahadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdatahadoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdatahadoopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lh/bigdatahadoop/BigdatahadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.bigdatahadoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdatahadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
